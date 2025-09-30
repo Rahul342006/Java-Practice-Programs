00599669dd44d3455b2469d1be7f8f0434d0c201
Rahul <rax2006ms@gmail.com>
2025-10-01 00:34:53 +0530

    Add files via upload

---
+++ Practice Set1/P5.java
@@ -0,0 +1,17 @@
+//write a java program to encrypt a grade by adding 8 and decrypt it
+import java.util.Scanner;
+public class P5{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter grade:");
+        char grade=sc.next().charAt(0);
+        int encryptedgrade=(grade+8);
+        System.out.println("Encrypted grade is:"+ encryptedgrade);
+        char decryptgrade=(char)(encryptedgrade-8);
+        System.out.println(" Decrypted grade is:" + decryptgrade);
+
+
+
+    }
+    
+}
